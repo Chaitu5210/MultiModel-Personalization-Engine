7b38f8f64239bc4d8affa4ee0e3fbcf94ecb08a3
Chaitanya-Varma-Rudraraju <140299567+Chaitu5210@users.noreply.github.com>
2024-10-05 20:58:23 +0530

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,5 @@
+from TextToFeatures import FindFeatures
+while(True):
+    print("What Would You Like To Try Now??")
+    Query=str(input())
+    FindFeatures(Query)
---
+++ TextToFeatures.py
@@ -0,0 +1,7 @@
+# This Module Takes The input from the Main File(Model.py) and then extract the features From the text and return them to the main file(Model) 
+from transformers import AutoTokenizer,AutoModelForCausalLM
+from transformers import pipeline
+
+def FindFeatures(Query) -> str:
+    return
+    
